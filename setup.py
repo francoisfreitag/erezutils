d80fee6a96232f8f3dfba06a43af83c332380cfd
Jon Dufresne <jon.dufresne@gmail.com>
2019-03-14 15:50:38 -0700

    Add python_requires to setup.py
    Reviewed-by: François Freitag

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.1.0",
     py_modules=["erezutils"],
     install_requires=["boto3"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
