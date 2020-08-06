fa922c20f1c8db9899d1394a1288f7c8b31af724
Jon Dufresne <jon.dufresne@gmail.com>
2020-08-06 12:35:10 -0700

    Use setuptools declarative syntax
    https://setuptools.readthedocs.io/en/latest/setuptools.html#configuring-setup-using-setup-cfg-files
    
    Reduces the mixing code and configuration by using a no-logic
    declarative syntax.

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="erezutils",
-    version="1.1.0",
-    py_modules=["erezutils"],
-    install_requires=["boto3"],
-    python_requires=">=3.6",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-    ],
-)
+setup()
